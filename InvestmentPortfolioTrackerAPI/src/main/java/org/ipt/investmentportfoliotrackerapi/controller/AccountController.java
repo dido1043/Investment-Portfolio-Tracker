af874e4f2b7ac52b72202e604957ad86feeb0784
dido1043 <dudunovski_05@abv.bg>
2026-05-12 22:05:10 +0300

    Implement CRUD operations for Account: add get, update, and delete endpoints

--- InvestmentPortfolioTrackerAPI/src/main/java/org/ipt/investmentportfoliotrackerapi/controller/AccountController.java
+++ InvestmentPortfolioTrackerAPI/src/main/java/org/ipt/investmentportfoliotrackerapi/controller/AccountController.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import org.ipt.investmentportfoliotrackerapi.data.dto.AccountDto;
 import org.ipt.investmentportfoliotrackerapi.service.AccountService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/account")
@@ -14,8 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     private final AccountService accountService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccount(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
     @PostMapping("/create")
     public ResponseEntity<AccountDto> createAccount(AccountDto accountDto) {
         return ResponseEntity.ok(accountService.createAccount(accountDto));
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id, @RequestBody AccountDto accountDto) {
+        return ResponseEntity.ok(accountService.updateAccount(id,accountDto));
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.deleteAccount(id));
     }
 }
