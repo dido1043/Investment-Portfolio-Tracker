0a661449e7ea39e5cbd40c7c77492b1849d166ff
dido1043 <dudunovski_05@abv.bg>
2026-05-19 22:42:35 +0300

    Enhance AccountRepository and JwtAuthenticationFilter: add findById method to AccountRepository and handle UsernameNotFoundException in JwtAuthenticationFilter

--- InvestmentPortfolioTrackerAPI/src/main/java/org/ipt/investmentportfoliotrackerapi/filter/JwtAuthenticationFilter.java
+++ InvestmentPortfolioTrackerAPI/src/main/java/org/ipt/investmentportfoliotrackerapi/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -38,7 +39,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String username = jwtService.extractUsername(jwt);
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            UserDetails userDetails;
+            try {
+                userDetails = userDetailsService.loadUserByUsername(username);
+            } catch (UsernameNotFoundException e) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
