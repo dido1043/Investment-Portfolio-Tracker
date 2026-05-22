4b89a0c058e08604f86ebabffdae34c561b82c4c
dido1043 <dudunovski_05@abv.bg>
2026-05-22 11:10:29 +0300

    Init frontend and created Register and Login frontend

---
+++ client/investment_portfolio/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Investment Portfolio Tracker</h1>
+            <p>Track your investments and manage your portfolio with ease.</p>
+        </div>
+    );
+}
+
+export default HomePage;
